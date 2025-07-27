3651ac147a389429de208f2751dba6699b7881f8
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2025-07-27 12:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var counter = 0;
+
+function increment() {
+	const elm = document.getElementById("counter_val");
+	counter++;
+	elm.textContent = counter;
+	alert(counter);
+}
